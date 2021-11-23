6a44c862a850ff96eb89bc44b843b33d7610076a
Maya Copeland <maya.copeland@protonmail.com>
2021-11-23 17:58:16 +0100

    Use f-strings instead of concatenation, order the list and take the first n out of it.

--- ILAGradeCalculator.py
+++ ILAGradeCalculator.py
@@ -1,18 +1,16 @@
 def remove_n_smallest(lst, n):
-    for _ in range(n):
-        m = min(lst)
-        lst.remove(m)
+	return sorted(lst)[n:]
 
 def WeeklyQuizzes (arr):
 	listr = list(map(float, arr))
-	remove_n_smallest(listr,2)
-	return (sum(listr) / (len(listr)*10))*40
+	grades = remove_n_smallest(listr,2)
+	return (sum(float(i) for i in grades) / (len(grades)*10))*40
 
 def Handins(arr):
 	listr = list(map(float, arr))
 	listdiv = [i/2 for i in listr]
-	remove_n_smallest(listdiv,1)
-	return float(sum(float(i) for i in listdiv)/30)*100
+	grades = remove_n_smallest(listdiv,1)
+	return float(sum(float(i) for i in grades)/30)*100
 # TODO: Add Synoptic Test
 def Synoptic(p):
 	return 30 * float(p/100)
@@ -33,7 +31,7 @@ def AnnoyingGradeBoundaries(g):
 	else:
 		return "Failing"
 
-def Main():
+def main():
 	week = input("what week of ILA have you last completed \n")
 	handins = input("how many hand-ins have you done \n")
 	has_synoptic = input("have you done the synoptic yet: Y/n \n")
@@ -53,21 +51,21 @@ def Main():
 	weekly_results = WeeklyQuizzes(quizzes)
 	handin_results = Handins(handin_result_list)
 	print("Your grade is:")
-	print("For Weekly Quizzes, you have "+str(weekly_results)+"% out of the 40% available")
-	print("For Hand-ins, you have"+str(float(handin_results/100)*30)+"% out of the 30% available")
+	print(f"For Weekly Quizzes, you have {str(weekly_results)}% out of the 40% available")
+	print(f"For Hand-ins, you have {str(float(handin_results/100)*30)}% out of the 30% available")
 	# get results, and get ready for your mood to come tumbling down, tumbling down, tumbling down. https://www.youtube.com/watch?v=rQiHzcdUPAU
 	# why are you reading the comments and why did i add something unfunny to it
-		# oh well, hi!
+	# oh well, hi!
 	if synoptic:
 		synoptic_results = Synoptic(synoptic)
-		print("For the synoptic test, you have"+str(synoptic_results*100)+"%")
-		print("Your total is:"+str((weekly_results+(float(handin_results/100)*30)+synoptic_results))+"%")
-		print("Your Grade Boundary is: "+AnnoyingGradeBoundaries((weekly_results+(float(handin_results/100)*30)+synoptic_results))+" out of the 30% available")
+		print(f"For the synoptic test, you have {str(synoptic_results*100)}%")
+		print(f"Your total is: {str((weekly_results+(float(handin_results/100)*30)+synoptic_results))}%")
+		print(f"Your Grade Boundary is: {AnnoyingGradeBoundaries((weekly_results+(float(handin_results/100)*30)+synoptic_results))} out of the 30% available")
 	else:
 		if week == 10:
 			print("ignore the scaling now")
-		print("Your current total is: "+str((weekly_results+(float(handin_results/100)*30)))+"% out of your available 70% (scaled to 100% it would be:" +str((weekly_results+(float(handin_results/100)*30))*1.42857142857))
-		print("Your Grade Boundary is: "+AnnoyingGradeBoundaries((weekly_results+(float(handin_results/100)*30))) + " scaled: " + AnnoyingGradeBoundaries((weekly_results+(float(handin_results/100)*30))*1.42857142857))
+		print(f"Your current total is: {str((weekly_results+(float(handin_results/100)*30)))}% out of your available 70% (scaled to 100% it would be: {str((weekly_results+(float(handin_results/100)*30))*1.42857142857)}")
+		print(f"Your Grade Boundary is: {AnnoyingGradeBoundaries((weekly_results+(float(handin_results/100)*30)))} scaled: {AnnoyingGradeBoundaries((weekly_results+(float(handin_results/100)*30))*1.42857142857)}")
 if __name__ == '__main__':
-	Main()
+	main()
 
